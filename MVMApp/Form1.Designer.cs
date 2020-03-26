d1d6430599fa0ef195ab8368aca6e6c903e4ef18
abba <abba@nuclearinstruments.eu>
2020-03-26 22:01:46 +0100

    test software for ESP32 MVM

---
+++ MVMApp/Form1.Designer.cs
@@ -0,0 +1,401 @@
+﻿namespace MVMApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.bRun = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown5 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown6 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown7 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown7)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 27.51773F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 72.48227F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(923, 451);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(256, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(664, 445);
+            this.panel1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.numericUpDown6);
+            this.panel2.Controls.Add(this.numericUpDown7);
+            this.panel2.Controls.Add(this.numericUpDown5);
+            this.panel2.Controls.Add(this.numericUpDown4);
+            this.panel2.Controls.Add(this.numericUpDown3);
+            this.panel2.Controls.Add(this.numericUpDown2);
+            this.panel2.Controls.Add(this.numericUpDown1);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.bRun);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(247, 445);
+            this.panel2.TabIndex = 1;
+            // 
+            // bRun
+            // 
+            this.bRun.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bRun.ForeColor = System.Drawing.Color.Red;
+            this.bRun.Location = new System.Drawing.Point(60, 28);
+            this.bRun.Name = "bRun";
+            this.bRun.Size = new System.Drawing.Size(120, 47);
+            this.bRun.TabIndex = 0;
+            this.bRun.Text = "RUN";
+            this.bRun.UseVisualStyleBackColor = true;
+            this.bRun.Click += new System.EventHandler(this.bRun_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Inhale (ms)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Exale (ms)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Inhale Pressure";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Exale Pressure";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 255);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Pressure Drop";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(9, 285);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(102, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Inhale max time (ms)";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(9, 312);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(105, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Exhale max time (ms)";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(104, 233);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "ALARMS";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(120, 110);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            15000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(98, 20);
+            this.numericUpDown1.TabIndex = 9;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            3000,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(120, 136);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            15000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(98, 20);
+            this.numericUpDown2.TabIndex = 10;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1500,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.DecimalPlaces = 2;
+            this.numericUpDown3.Location = new System.Drawing.Point(120, 176);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            15000,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(98, 20);
+            this.numericUpDown3.TabIndex = 11;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.DecimalPlaces = 2;
+            this.numericUpDown4.Location = new System.Drawing.Point(120, 202);
+            this.numericUpDown4.Maximum = new decimal(new int[] {
+            15000,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(98, 20);
+            this.numericUpDown4.TabIndex = 12;
+            this.numericUpDown4.Value = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown5
+            // 
+            this.numericUpDown5.DecimalPlaces = 2;
+            this.numericUpDown5.Location = new System.Drawing.Point(120, 253);
+            this.numericUpDown5.Maximum = new decimal(new int[] {
+            15000,
+            0,
+            0,
+            0});
+            this.numericUpDown5.Name = "numericUpDown5";
+            this.numericUpDown5.Size = new System.Drawing.Size(98, 20);
+            this.numericUpDown5.TabIndex = 13;
+            this.numericUpDown5.Value = new decimal(new int[] {
+            85,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown6
+            // 
+            this.numericUpDown6.Location = new System.Drawing.Point(120, 283);
+            this.numericUpDown6.Maximum = new decimal(new int[] {
+            15000,
+            0,
+            0,
+            0});
+            this.numericUpDown6.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown6.Name = "numericUpDown6";
+            this.numericUpDown6.Size = new System.Drawing.Size(98, 20);
+            this.numericUpDown6.TabIndex = 15;
+            this.numericUpDown6.Value = new decimal(new int[] {
+            5000,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown7
+            // 
+            this.numericUpDown7.Location = new System.Drawing.Point(120, 310);
+            this.numericUpDown7.Maximum = new decimal(new int[] {
+            15000,
+            0,
+            0,
+            0});
+            this.numericUpDown7.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown7.Name = "numericUpDown7";
+            this.numericUpDown7.Size = new System.Drawing.Size(98, 20);
+            this.numericUpDown7.TabIndex = 14;
+            this.numericUpDown7.Value = new decimal(new int[] {
+            6000,
+            0,
+            0,
+            0});
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(143, 357);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Program";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(923, 451);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown7)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.NumericUpDown numericUpDown6;
+        private System.Windows.Forms.NumericUpDown numericUpDown7;
+        private System.Windows.Forms.NumericUpDown numericUpDown5;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bRun;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ MVMApp/Form1.cs
@@ -0,0 +1,196 @@
+﻿using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.WindowsForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+
+namespace MVMApp
+{
+    public partial class Form1 : Form
+    {
+        string URL = "http://192.168.1.52/";
+        public class PressureReading
+        {
+            public double pressure { get; set; }
+            public string id { get; set; }
+            public string name { get; set; }
+            public string hardware { get; set; }
+            public bool connected { get; set; }
+        }
+
+
+        public PlotModel myModel;
+        LineSeries ss;
+        PlotView myPlot;
+        PlotModel model;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            myPlot = new PlotView();
+
+            //Create Plotmodel object
+            model = new PlotModel() { LegendSymbolLength = 24 };
+             ss = new LineSeries()
+            {
+                Color = OxyColors.SkyBlue,
+                MarkerType = MarkerType.Circle,
+                MarkerSize = 2,
+                MarkerStroke = OxyColors.White,
+                MarkerFill = OxyColors.SkyBlue,
+                MarkerStrokeThickness = 1.5
+            };
+
+            
+
+
+            model.Series.Add(ss);
+            //Assign PlotModel to PlotView
+            myPlot.Model = model;
+
+            //Set up plot for display
+            myPlot.Dock = System.Windows.Forms.DockStyle.Bottom;
+            myPlot.Location = new System.Drawing.Point(0, 0);
+            myPlot.Size = new System.Drawing.Size(500, 500);
+            myPlot.TabIndex = 0;
+            myPlot.Dock = DockStyle.Fill;
+            //Add plot control to form
+            panel1.Controls.Add(myPlot);
+
+            timer1.Enabled = true;
+        }
+        int ttt=0;
+        double mmin, mmax;
+
+        private void bRun_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            System.Threading.Thread.Sleep(500);
+            if (bRun.Tag == "RUN")
+            {
+                bRun.Text = "RUN";
+                bRun.Tag = "STOP";
+
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL + "run?params=0");
+                request.ContentType = "application/json; charset=utf-8";
+                HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+                response.Close();
+            }
+            else
+            {
+                bRun.Text = "STOP";
+                bRun.Tag = "RUN";
+
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL + "run?params=1");
+                request.ContentType = "application/json; charset=utf-8";
+                HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+                response.Close();
+            }
+            timer1.Enabled = true;
+        }
+
+        private void set_param(String Param, String Value)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL + Param+ "?params=" + Value);
+            request.Timeout = 1000;
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+            response.Close();
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            System.Threading.Thread.Sleep(500);
+            set_param("inhale_ms", numericUpDown1.Value.ToString().Replace(",", "."));
+            System.Threading.Thread.Sleep(100);
+            set_param("exhale_ms", numericUpDown2.Value.ToString().Replace(",", "."));
+            System.Threading.Thread.Sleep(100);
+            set_param("inhale_alarm_ms", numericUpDown7.Value.ToString().Replace(",", "."));
+            System.Threading.Thread.Sleep(100);
+            set_param("exhale_alarm_ms", numericUpDown6.Value.ToString().Replace(",", "."));
+            System.Threading.Thread.Sleep(100);
+            set_param("pressure_max", numericUpDown3.Value.ToString().Replace(",", "."));
+            System.Threading.Thread.Sleep(100);
+            set_param("pressure_min", numericUpDown4.Value.ToString().Replace(",", "."));
+            System.Threading.Thread.Sleep(100);
+            set_param("pressure_drop", numericUpDown5.Value.ToString().Replace(",", "."));
+            System.Threading.Thread.Sleep(100);
+            MessageBox.Show("Done");
+            timer1.Enabled = true;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL + "pressure");
+            request.Timeout = 300;
+            request.ContentType = "application/json; charset=utf-8";
+            try
+            {
+
+            
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+            using (Stream responseStream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                String data = reader.ReadToEnd();
+                    response.Close();
+                Console.WriteLine(data);
+                try
+                {
+                    PressureReading psr = JsonConvert.DeserializeObject<PressureReading>(data);
+
+                    mmin = Math.Min((double)mmin, (double)psr.pressure);
+                    mmax = Math.Max((double)mmax, (double)psr.pressure);
+                    model.DefaultYAxis.Zoom(mmin-0.05* Math.Abs(mmin), mmax+0.05* Math.Abs(mmax));
+
+                    ss.Points.Add(new DataPoint(ttt, psr.pressure));
+                    ttt++;
+
+                    if (ttt > 90)
+                    {
+                        double panStep = model.DefaultXAxis.Transform(-1 + model.DefaultXAxis.Offset);
+                        model.DefaultXAxis.Pan(panStep);
+                    }
+                    myPlot.Refresh();
+          
+
+                } catch (Exception ex)
+                {
+                
+
+                }
+                
+            }
+            }
+            catch (Exception ex)
+            {
+
+                
+            }
+
+
+
+
+
+        }
+    }
+}
